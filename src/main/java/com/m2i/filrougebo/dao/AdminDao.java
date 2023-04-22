1ee8b95e9af46ae055ffd1533fce328017aa33bc
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-04-22 18:24:03 +0200

    test commit

--- src/main/java/com/m2i/filrougebo/dao/AdminDao.java
+++ src/main/java/com/m2i/filrougebo/dao/AdminDao.java
@@ -1,7 +1,30 @@
 package com.m2i.filrougebo.dao;
 
+import java.util.List;
+
 public class AdminDao implements IntAdminDao{
+    @Override
+    public boolean create(Object entity) {
+        return false;
+    }
 
+    @Override
+    public List findAll() {
+        return null;
+    }
 
+    @Override
+    public Object findById(Object o) {
+        return null;
+    }
 
+    @Override
+    public void update(Object entity) {
+
+    }
+
+    @Override
+    public void delete(Object entity) {
+
+    }
 }
