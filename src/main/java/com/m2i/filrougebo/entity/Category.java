72317e001e86363fc5c516a85de95dbcde34a2a9
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-04-22 18:36:43 +0200

    test new branch commit

--- src/main/java/com/m2i/filrougebo/entity/Category.java
+++ src/main/java/com/m2i/filrougebo/entity/Category.java
@@ -1,47 +1,4 @@
 package com.m2i.filrougebo.entity;
 
-import java.util.List;
-
 public class Category {
-    private int idCategory;
-    private String name;
-    private List<Product> products;
-
-    public Category(int idCategory, String name, List<Product> products) {
-        this.idCategory = idCategory;
-        this.name = name;
-        this.products = products;
-    }
-
-    public Category(String name, List<Product> products) {
-        this.name = name;
-        this.products = products;
-    }
-
-    public Category() {
-    }
-
-    public int getIdCategory() {
-        return idCategory;
-    }
-
-    public void setIdCategory(int idCategory) {
-        this.idCategory = idCategory;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
 }
