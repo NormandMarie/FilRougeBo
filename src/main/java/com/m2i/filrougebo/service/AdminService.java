25a60ab89ae4c3bca2a4ef4f69fd2ea3064ae49c
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-04-30 21:13:51 +0200

    created admin service crud

--- src/main/java/com/m2i/filrougebo/service/AdminService.java
+++ src/main/java/com/m2i/filrougebo/service/AdminService.java
@@ -2,7 +2,27 @@ package com.m2i.filrougebo.service;
 
 import com.m2i.filrougebo.dao.AdminDao;
 import com.m2i.filrougebo.dao.IntAdminDao;
+import com.m2i.filrougebo.entity.Admin;
+
+import java.util.List;
 
 public class AdminService {
-    private IntAdminDao adminDao = new AdminDao();
+    private static IntAdminDao adminDao = new AdminDao();
+
+    public Admin create(Admin admin){
+        return adminDao.create(admin);
+    }
+    public List<Admin> findAll(){
+        return adminDao.findAll();
+    }
+    public Admin findById(int id){
+        return adminDao.findById(id);
+    }
+    public boolean update(Admin admin){
+        return adminDao.update(admin);
+    }
+    public boolean delete(Admin admin){
+        return adminDao.delete(admin);
+    }
+
 }
